fbb367aab9adb3312c7c4347a0b3ff591fbc790d
Andriikryl <andreyk557@gmail.com>
2024-02-02 19:34:06 +0200

    add AddTodo component

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
   import type { ITodo } from "../types/todo";
+  import AddTodo from "./AddTodo.svelte";
+  $: todosAmount = todos.length;
+  function generateRandomId(): string {
+    return Math.random().toString(16).slice(2);
+  }
+  function addTodo(todo: string): void {
+    let newTodo: ITodo = {
+      id: generateRandomId(),
+      text: todo,
+      completed: false,
+    };
+    todos = [...todos, newTodo];
+  }
+  function toggleCompleted(event: MouseEvent): void {
+    let { checked } = event.target as HTMLInputElement;
+
+    todos = todos.map((todo) => ({
+      ...todo,
+      completed: checked,
+    }));
+  }
+
   let todos: ITodo[] = [
     { id: "1e4a59703af84", text: "Todo 1", completed: true },
     { id: "9e09bcd7b9349", text: "Todo 2", completed: false },
@@ -12,44 +34,39 @@
   <h1 class="title">todos</h1>
 
   <section class="todos">
-    <form>
-      <input type="checkbox" id="toggle-all" class="toggle-all" />
-      <label aria-label="Mark all as complete" for="toggle-all">
-        Mark all as complete
-      </label>
+    <AddTodo {addTodo} {toggleCompleted} {todosAmount} />
+    {#if todosAmount}
+      <ul class="todo-list">
+        {#each todos as todo (todo.id)}
+          <li class="todo">
+            <div class="todo-item">
+              <div>
+                <input
+                  checked={todo.completed}
+                  id="todo"
+                  class="toggle"
+                  type="checkbox"
+                />
+                <label aria-label="Check todo" class="todo-check" for="todo" />
+              </div>
+              <span class="todo-text">{todo.text}</span>
+              <button aria-label="Remove todo" class="remove" />
+            </div>
+            <!-- <input class="edit" type="text" autofocus /> -->
+          </li>
+        {/each}
+      </ul>
 
-      <input
-        id="new-todo"
-        class="new-todo"
-        placeholder="What needs to be done?"
-        type="text"
-      />
-    </form>
-
-    <ul class="todo-list">
-      <li class="todo">
-        <div class="todo-item">
-          <div>
-            <input id="todo" class="toggle" type="checkbox" />
-            <label aria-label="Check todo" class="todo-check" for="todo" />
-          </div>
-          <span class="todo-text">Todo 1</span>
-          <button aria-label="Remove todo" class="remove" />
+      <div class="actions">
+        <span class="todo-count">0 left</span>
+        <div class="filters">
+          <button class="filter">All</button>
+          <button class="filter">Active</button>
+          <button class="filter">Completed</button>
         </div>
-
-        <!-- <input class="edit" type="text" autofocus /> -->
-      </li>
-    </ul>
-
-    <div class="actions">
-      <span class="todo-count">0 left</span>
-      <div class="filters">
-        <button class="filter">All</button>
-        <button class="filter">Active</button>
-        <button class="filter">Completed</button>
+        <button class="clear-completed">Clear completed</button>
       </div>
-      <button class="clear-completed">Clear completed</button>
-    </div>
+    {/if}
   </section>
 </main>
 
