80fc7771362a5b62e1e03ebb1aea211f172a9768
Andriikryl <andreyk557@gmail.com>
2024-02-02 18:53:16 +0200

    add types

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,5 +1,6 @@
-<script>
-  let todos = [
+<script lang="ts">
+  import type { ITodo } from "../types/todo";
+  let todos: ITodo[] = [
     { id: "1e4a59703af84", text: "Todo 1", completed: true },
     { id: "9e09bcd7b9349", text: "Todo 2", completed: false },
     { id: "9e4273a51a37c", text: "Todo 3", completed: false },
